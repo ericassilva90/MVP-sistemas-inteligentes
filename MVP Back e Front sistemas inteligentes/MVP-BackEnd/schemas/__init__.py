379db87fb29f317eb93755ba69afe10718cca486
ericassilva90 <ericasantana@live.com>
2025-07-08 06:01:10 -0300

    Delete MVP Back e Front sistemas inteligentes/MVP-BackEnd directory

--- MVP Back e Front sistemas inteligentes/MVP-BackEnd/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from schemas.livro import BookSchema, BookViewSchema, \
-                            ListBookSchema, \
-                            apresenta_livro, apresenta_livros
-from schemas.error import ErrorSchema
