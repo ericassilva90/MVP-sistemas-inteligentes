c694d68d6b643585d9581168d2a88fd2e0287136
ericassilva90 <ericasantana@live.com>
2025-07-08 05:45:53 -0300

    __init__.py

--- MVP Back e Front sistemas inteligentes/MVP-BackEnd/schemas/__init__.py
+++ MVP Back e Front sistemas inteligentes/MVP-BackEnd/schemas/__init__.py
@@ -1,5 +1,5 @@
 
-from schemas.livro import LivroSchema, LivroBuscaSchema, LivroViewSchema, \
-                            ListagemLivrosSchema, LivroDeleteSchema, AutorBuscaSchema, \
+from schemas.livro import BookSchema, BookViewSchema, \
+                            ListBookSchema, \
                             apresenta_livro, apresenta_livros
 from schemas.error import ErrorSchema
